d8e369734c6be73b2a20aa3ea98f72cb07b1a22b
philipp012 <philipp.kiss@bluewin.ch>
2019-02-06 15:28:58 +0100

    added gitignore

--- src/sample/Login.java
+++ src/sample/Login.java
@@ -1,7 +1,32 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Login {
+    public Button btLogou;
+
     void init() {
 
+    }
+
+    public void logOut() throws IOException {
+        Stage stageToClose = (Stage) btLogou.getScene().getWindow();
+        stageToClose.close();
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("sample.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 500);
+        ((Controller) fxmlLoader.getController()).init();
+
+        Stage sample = new Stage();
+        sample.setTitle("Login");
+        sample.setScene(scene);
+        sample.setResizable(false);
+        sample.show();
     }
 }
